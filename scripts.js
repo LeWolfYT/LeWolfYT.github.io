f0010fa2a3522038b131dfdcbbca8bc896d8d7b9
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-01-06 15:16:28 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,4 +3,5 @@ function bee() {
   alert("According to all known laws of aviation,");
   alert("there is no way a bee should be able to fly.");
   alert("Its wings are too small to get its fat little body off the ground.");
+  alert("The bee, of course, flies anyway");
 }
