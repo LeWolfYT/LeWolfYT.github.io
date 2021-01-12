ac055c807523cabf10d7548a438402c106ca1e69
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-01-12 14:34:46 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 console.log("Hi!!!1!!!! i love coding");
 var drk = false;
 function alert() {
-alert("Welcome to the website! I hope you have a great time here!")
-);
+alert("Welcome to the website! I hope you have a great time here!");
+}
 function bee() {
   alert("According to all known laws of aviation,");
   alert("there is no way a bee should be able to fly.");
   alert("Its wings are too small to get its fat little body off the ground.");
   alert("The bee, of course, flies anyway");
-  console.log("e")
-};
+  console.log("e");
+}
 function dark() {
-  if (drk == false) {
+  if (drk === false) {
     alert("In progress, does nothing");
-    var drk = true;
-  } else if (drk == true) {
+    drk = true;
+  } else if (drk === true) {
     alert("still does nothing");
-    var drk = false;
+    drk = false;
   }
 }
