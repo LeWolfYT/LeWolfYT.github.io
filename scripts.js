66e732a6b810c7f8cc9cd99a73204a4dff7a8c43
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-01-12 14:28:12 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 console.log("Hi!!!1!!!! i love coding");
-var drk = false
+var drk = false;
 function alert() {
 alert("Welcome to the website! I hope you have a great time here!")
-)
+);
 function bee() {
   alert("According to all known laws of aviation,");
   alert("there is no way a bee should be able to fly.");
   alert("Its wings are too small to get its fat little body off the ground.");
   alert("The bee, of course, flies anyway");
   console.log("e")
-}
+};
 function dark() {
   if (drk == false) {
     alert("In progress, does nothing");
