30b7670034b5856030f24538b2107cff1e1c7519
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-01-12 14:13:17 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 console.log("Hi!!!1!!!! i love coding");
+function alert() {
 alert("Welcome to the website! I hope you have a great time here!")
+)
 function bee() {
   alert("According to all known laws of aviation,");
   alert("there is no way a bee should be able to fly.");
