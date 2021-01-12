e494e2b0863afd0fc88ffe83c19fa0ee4cd3d057
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2021-01-12 14:23:40 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 console.log("Hi!!!1!!!! i love coding");
+var drk = false
 function alert() {
 alert("Welcome to the website! I hope you have a great time here!")
 )
@@ -8,4 +9,13 @@ function bee() {
   alert("Its wings are too small to get its fat little body off the ground.");
   alert("The bee, of course, flies anyway");
   console.log("e")
+}
+function dark() {
+  if (drk == false) {
+    alert("In progress, does nothing");
+    var drk = true;
+  } else if (drk == true) {
+    alert("still does nothing");
+    var drk = false;
+  }
 }
