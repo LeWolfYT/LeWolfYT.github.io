eac88930a09959e76fa0e3e6890691812948faa2
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-05-04 19:56:28 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,9 @@
-console.log("Hi!!!1!!!! i love coding");
-var drk = false;
-function alert() {
-alert("Welcome to the website! I hope you have a great time here!");
-}
 function bee() {
   alert("According to all known laws of aviation,");
   alert("there is no way a bee should be able to fly.");
   alert("Its wings are too small to get its fat little body off the ground.");
   alert("The bee, of course, flies anyway");
-  console.log("e");
 }
 function dark() {
-  if (drk === false) {
-    alert("In progress, does nothing");
-    drk = true;
-  } else if (drk === true) {
-    alert("still does nothing");
-    drk = false;
-  }
+  alert("I finally updated this website after 16 months!")
 }
