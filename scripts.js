3a5da05e2c83461ccf8a51afc3344e7ec91501a4
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-05-04 19:59:49 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 function bee() {
-  alert("According to all known laws of aviation,");
-  alert("there is no way a bee should be able to fly.");
-  alert("Its wings are too small to get its fat little body off the ground.");
-  alert("The bee, of course, flies anyway");
+  alert('According to all known laws of aviation,');
+  alert('there is no way a bee should be able to fly.');
+  alert('Its wings are too small to get its fat little body off the ground.');
+  alert('The bee, of course, flies anyway');
 }
 function dark() {
-  alert("I finally updated this website after 16 months!")
+  alert('I finally updated this website after 16 months!')
 }
