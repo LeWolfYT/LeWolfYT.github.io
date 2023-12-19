667fedaabf59d3a70715243d7300ed6f9a1eb693
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-12-19 08:44:24 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ for (let i=0; i < pjs.length; i++) {
 }
 await sleep(250);
 document.title = "\u200E" + pjs;
-await sleep(5000);
+await sleep(pjs.length()*500);
 for (let i=pjs.length; i > -1; i--) {
   tx = tx.slice(0, -1);
   await sleep(250);
