d47f060df1b8ef181a345f7d15d743fe005c8d46
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2024-05-15 08:48:08 -0400

    Update rss.js

--- rss.js
+++ rss.js
@@ -4,7 +4,7 @@ function wrapHtml(input, wrapper, extra = "") {
 
 $.ajax({
 	type: 'GET',
-	url: "feeds/rss.xml",
+	url: "https://lewolfyt.github.io/feeds/rss.xml",
 	crossDomain: true,
 	dataType: 'XML',
 	success: function (xml) {
